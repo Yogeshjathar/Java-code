23cbe83d52028a4fc3ede43c7114b3ae4cb4ad09
Yogesh Jathar <109969909+Yogeshjathar@users.noreply.github.com>
2025-12-04 16:10:28 +0530

    Update MaxSubArraySum.java
    added code which incudes approach from brute force to optimized

--- 2D Array/MaxSubArraySum.java
+++ 2D Array/MaxSubArraySum.java
@@ -1,19 +1,33 @@
 public class MaxSubArraySum {
     public static void printSubArray_BruteForce(int[] arr){
-        int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
         for(int i=0; i<arr.length; i++){
-            for(int j=i+1; j<arr.length; j++){
-                currSum =0;
-                for(int k=i; k<=j; k++){
-                    currSum += arr[k];
-                }
-                if(maxSum < currSum){
-                    maxSum = currSum;
-                }
+            int currSum = 0;
+            for(int j=i; j<arr.length; j++){
+                currSum += arr[j];
+                maxSum = Math.max(maxSum, currSum);
             }
         }
         System.out.println("Maximum sum of Subarray using Brute Force: "+maxSum);
+    }
+
+    public static int printSubArray_Prefixsum(int arr[]){
+        int n = arr.length;
+        int ms = Integer.MIN_VALUE;
+        int prefix[] = new int[n];
+
+        prefix[0] = arr[0];
+        for(int i=1; i<n; i++){
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                int curr_sum = i==0 ? prefix[j] : prefix[j] - prefix[i-1];
+                ms = Math.max(curr_sum, ms);
+            }
+        }
+        return ms;
     }
 
     public static int printSubArray_Kadanes(int[] arr){
@@ -29,6 +43,19 @@ public class MaxSubArraySum {
             maxElemet = Math.max(maxElemet,arr[i]);
         }
         return ms == 0 ? maxElemet : ms;
+    }
+
+    // both are same but boilerplate code is reduced
+    public static int maxSubArray_Kadanes(int[] nums) {
+        int cs = nums[0];
+        int ms = nums[0];
+
+        for(int i=1; i<nums.length; i++){
+            cs = Math.max(nums[i], cs + nums[i]);
+            ms = Math.max(cs, ms);
+        }
+
+        return ms;
     }
 
     public static void main(String args[]){
