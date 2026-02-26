9f41efe6ece6e0774026ebcecdaba4894cb73456
Yogesh Jathar <109969909+Yogeshjathar@users.noreply.github.com>
2026-02-26 11:42:15 +0530

    Selection Sort Algo

---
+++ Advance/sorting_algo/SelectionSort.java
@@ -0,0 +1,34 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        selectionSort(arr);
+        System.out.println("Sorted array: ");
+        printArray(arr);
+    }
+
+    // Time Complexity: O(n^2) in all cases
+    // logic is to find the minimum element in unsorted array and swap it with the first element of unsorted array
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // swap minIndex and i
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
