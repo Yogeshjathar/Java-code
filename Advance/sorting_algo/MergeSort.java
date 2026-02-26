319f28d73db354896d432c1d8aa5d8439fc7069c
Yogesh Jathar <109969909+Yogeshjathar@users.noreply.github.com>
2026-02-26 12:20:06 +0530

    Merge Sort Algo

---
+++ Advance/sorting_algo/MergeSort.java
@@ -0,0 +1,54 @@
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println("Sorted array: ");
+        printArray(arr);
+    }
+
+    // Time Complexity: O(n log n) in all cases
+    // logic is to divide the array into two halves, sort them recursively and then merge the sorted halves
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left > right) return;
+
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+        
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int temp[] = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+        
+        for (i = left; i <= right; i++) {
+            arr[i] = temp[i - left];
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
